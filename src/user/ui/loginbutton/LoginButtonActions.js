c5fdd0efe51389fded383af394e5aa7432833f2b
Bryce Doganer <bryce.doganer@gmail.com>
2019-02-16 17:18:13 -0700

    update login functionality

--- src/user/ui/loginbutton/LoginButtonActions.js
+++ src/user/ui/loginbutton/LoginButtonActions.js
@@ -1,5 +1,4 @@
 import { uport } from './../../../util/connectors.js'
-import { browserHistory } from 'react-router'
 
 export const USER_LOGGED_IN = 'USER_LOGGED_IN'
 function userLoggedIn(user) {
@@ -11,12 +10,13 @@ function userLoggedIn(user) {
 
 export function loginUser() {
   return function(dispatch) {
-    console.log('login action')
     console.log(uport)
 
     // UPort and its web3 instance are defined in ./../../../util/wrappers.
     // Request uPort persona of account passed via QR
-    uport.requestCredentials().then((credentials) => {
+    uport.requestCredentials({
+      requested: ['name']
+    }).then((credentials) => {
       dispatch(userLoggedIn(credentials))
 
       // Used a manual redirect here as opposed to a wrapper.
