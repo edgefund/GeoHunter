fbfdf746aafd3e0a9bbba8ff105b14143d9c1c40
Lucas Henning <lhenning@citizensreserve.com>
2019-02-17 06:50:50 -0700

    changed quorum node hostname

--- src/provider/quorumConfig.js
+++ src/provider/quorumConfig.js
@@ -1,6 +1,6 @@
 export default {
   privateKey: "15E0DB619B64C1E175CE0F0F05F21F19BFCAF2B1DDF1C47C88F7C778CFD47AF5",
   publicKey: "0xb8d442BF5A8844E27295B152E5e0124819459bB7",
-  providerEndpoint: 'http://ec2-34-220-53-37.us-west-2.compute.amazonaws.com:22000',
+  providerEndpoint: 'https://geohunter.duckdns.org:1337/',
   networkId: 10
 }
